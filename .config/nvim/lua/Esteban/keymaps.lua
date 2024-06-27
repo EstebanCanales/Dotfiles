e7e646aecdf44442c4371aeccde7e28fea874787
andromeda <jack.bright.director@gmail.com>
2024-06-27 03:06:16 +0000

    polybar

--- .config/nvim/lua/Esteban/keymaps.lua
+++ .config/nvim/lua/Esteban/keymaps.lua
@@ -29,10 +29,10 @@ end)
 
 -- Keymaps para moverse en la terminal
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
-vim.keymap.set("n", "<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Move left" })
-vim.keymap.set("n", "<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Move down" })
-vim.keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Move up" })
-vim.keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move right" })
+-- vim.keymap.set("n", "<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Move left" })
+-- vim.keymap.set("n", "<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Move down" })
+-- vim.keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Move up" })
+-- vim.keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move right" })
 
 
 -- Keymaps para hacer split
