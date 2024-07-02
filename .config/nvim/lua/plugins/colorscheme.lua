de8fb1f748c8ff3d6597caf60075217d865d56c9
andromeda <jack.bright.director@gmail.com>
2024-07-01 21:53:38 -0600

    no se

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,88 @@
-return {
-	'navarasu/onedark.nvim',
-	config = function()
-		require('onedark').setup {
-			style = 'darker',
-			  code_style = {
-        comments = 'italic',
-        keywords = 'none',
-        functions = 'italic',
-        strings = 'none',
-        variables = 'none'
-    },
-		}
+-- return {
+-- 	'navarasu/onedark.nvim',
+-- 	config = function()
+-- 		require('onedark').setup({
+-- 			style = 'dark',
+-- 			transparent = false,
+-- 			term_colors = true,
+-- 			ending_tildes = false,
+-- 			cmp_itemkind_reverse = false,
+--
+-- 			toggle_style_key = "<leader>st",
+-- 			toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
+-- 			code_style = {
+-- 				comments = 'italic',
+-- 				keywords = 'none',
+-- 				functions = 'italic',
+-- 				strings = 'none',
+-- 				variables = 'italic'
+-- 			},
+--
+-- 			lualine = {
+-- 				transparent = true,
+-- 			},
+--
+-- 		})
+-- 		require('onedark').load()
+-- 	end
+-- }
 
-		require('onedark').load()
+
+
+
+--
+-- SONOKAI
+--
+
+return {
+	'sainnhe/sonokai',
+	lazy = false,
+	priority = 1000,
+	config = function()
+
+		vim.g.sonokai_enable_italic = true
+		vim.cmd.colorscheme('sonokai')
 	end
 }
 
+
+--
+-- PONOKAI
+--
+-- return {
+--       'shawilly/ponokai',
+--       lazy = false,
+--       priority = 1000,
+--       config = function()
+--         -- Optionally configure and load the colorscheme
+--         -- directly inside the plugin declaration.
+--         vim.g.sonokai_enable_italic = true
+--         vim.cmd.colorscheme('ponokai')
+--       end
+--     }
+
+
+
+
+--
+-- ONE DARK PRO
+--
+
+-- return {
+-- 	'navarasu/onedark.nvim',
+-- 	config = function()
+-- 		require('onedark').setup {
+-- 			style = 'darker',
+-- 			  code_style = {
+--         comments = 'italic',
+--         keywords = 'none',
+--         functions = 'italic',
+--         strings = 'none',
+--         variables = 'none'
+--     },
+-- 		}
+--
+-- 		require('onedark').load()
+-- 	end
+-- }
+--
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,27 @@
 return {
-    	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    	dependencies = { 'nvim-lua/plenary.nvim' },
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.6',
+	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
+		require('telescope').setup {
+			defaults = {
+				file_ignore_patterns = { 'node_modules', '.git' },
+				prompt_prefix = '❯ ',
+				selection_caret = '❯ ',
+				layout_config = {
+					prompt_position = 'top',
+					horizontal = { width_padding = 0.1, height_padding = 0.1, preview_width = 0.6 },
+					vertical = { width_padding = 0.05, height_padding = 1, preview_height = 0.5 },
+				},
+				mappings = {
+					i = {
+						['<esc>'] = require('telescope.actions').close,
+						['<C-j>'] = require('telescope.actions').move_selection_next,
+						['<C-k>'] = require('telescope.actions').move_selection_previous,
+					},
+				},
+			},
+		}
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
