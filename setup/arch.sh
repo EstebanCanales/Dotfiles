1020b8411e4a4e7c3a546d47b3662214067f3baf
andromeda <jack.bright.director@gmail.com>
2024-07-17 19:58:27 -0600

    final

--- setup/arch.sh
+++ setup/arch.sh
@@ -1,4 +1,5 @@
 sudo pacman -S neovim zsh stow nodejs python lua luarocks neofetch \
 	gcc make cmake ripgrep kitty git unzip kmix pulseaudio \
 	alsa-utils npm python-pip picom rofi polybar xwallpaper rnote \
+
 	
