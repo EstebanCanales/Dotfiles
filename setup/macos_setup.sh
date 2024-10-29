1171f7074f169f8687bc979fcc3da6405c99932d
Esteban Canales <estebancomoprogramador@gmail.com>
2024-10-29 17:28:03 -0600

    add changes

--- setup/macos_setup.sh
+++ setup/macos_setup.sh
@@ -1,3 +1,6 @@
 brew install stow kitty koekeishiya/formulae/yabai koekeishiya/formulae/skhd \
 	canva figma keycastr obsidian raycast spotify thunderbird warp whatsapp \
-	unzip tree ripgrep php fastfetch
+	unzip tree ripgrep php fastfetch jq gh sketchybar switchaudio-osx
+
+brew install --cask sf-symbols
+brew tap FelixKratz/formulae
